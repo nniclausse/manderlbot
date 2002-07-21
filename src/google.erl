3a39d13da3948beba6292661ee06e84238cf8f49
dim <dim>
2002-07-21 11:34:49 +0000

    form

--- src/google.erl
+++ src/google.erl
@@ -9,40 +9,42 @@
 -export([search/1]).
 
 search(Keyword) ->
-	init({"www.google.com", 80, Keyword}).
+    init({"www.google.com", 80, Keyword}).
 
 do_recv(Socket) ->
-	do_recv(Socket, [], []).
+    do_recv(Socket, [], []).
 
 do_recv(Socket, Bs, URL) ->
-	case gen_tcp:recv(Socket, 0) of
+    case gen_tcp:recv(Socket, 0) of
         {ok, B} ->
-			%% search for redirected url
-			case regexp:first_match(B, "<A HREF=\"http://[^\"]+\">here</A>") of 
-				{match,Start,Length} -> % ok, found
-					do_recv(Socket, [Bs, B], string:substr(B, 10, length(B)-22));
-				_ -> 
-					do_recv(Socket, [Bs, B], URL)
-				end;
+	    %% search for redirected url
+	    case regexp:first_match(B, "<A HREF=\"http://[^\"]+\">here</A>") of
+		{match,Start,Length} -> % ok, found
+		    do_recv(Socket, [Bs, B],
+			    string:substr(B, 10, length(B)-22));
+		_ -> 
+		    do_recv(Socket, [Bs, B], URL)
+	    end;
         {error, einval} -> % non fatal error, continue
             do_recv(Socket, Bs, URL);
         {error, closed} -> 
             {ok, URL}
     end.
-	
+
 init({Server, Port, Keyword}) ->
     case gen_tcp:connect(Server, Port,
-						 [list,
-						  {packet, line},
-						  {active, false}]) of
-		{ok, Socket} -> 
-			Request = google_request(Keyword),
-			gen_tcp:send(Socket, Request),
-			{ok, URL} = do_recv(Socket),
-			URL;
-		{error, Reason} ->
-			{stop, connfailed}
+			 [list,
+			  {packet, line},
+			  {active, false}]) of
+	{ok, Socket} -> 
+	    Request = google_request(Keyword),
+	    gen_tcp:send(Socket, Request),
+	    {ok, URL} = do_recv(Socket),
+	    URL;
+	{error, Reason} ->
+	    {stop, connfailed}
     end.
 
 google_request(Keyword) ->
-	"GET /search?q=" ++ Keyword ++"&hl=fr&btnI=J%27ai+de+la+chance HTTP/1.0" ++ io_lib:nl() ++ io_lib:nl().
+    "GET /search?q=" ++ Keyword ++"&hl=fr&btnI=J%27ai+de+la+chance HTTP/1.0"
+	++ io_lib:nl() ++ io_lib:nl().
