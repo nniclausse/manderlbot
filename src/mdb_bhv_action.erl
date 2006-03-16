d4b2212dced4b0246d555db98b30d61b6244caa8
nniclausse <nniclausse>
2006-03-16 09:36:53 +0000

    fix: action/3 doesn't exists !

--- src/mdb_bhv_action.erl
+++ src/mdb_bhv_action.erl
@@ -40,14 +40,6 @@
 %%% Function: behaviour/5
 %%% Purpose:  Answer with Action IRC usage - /me
 %%%----------------------------------------------------------------------
-behaviour(Input = #data{header_to=BotName}, BotName, Data, BotPid, Channel) ->
-    [NickFrom|IpFrom] = string:tokens(Input#data.header_from, "!"),
-
-    lists:map(fun(String) ->
-		      mdb_bot:action(BotPid, String, NickFrom)
-	      end,
-	      Data);
-
 behaviour(Input, BotName, Data, BotPid, Channel) ->
     lists:map(fun(String) ->
 		      mdb_bot:action(BotPid, String)
